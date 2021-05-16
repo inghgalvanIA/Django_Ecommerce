b5d524e992d90dd751743f176c9d5158879dad3a
Hector Galvan <inghgalvan@gmail.com>
2021-05-16 01:00:26 -0500

    mitad implementado template bootstrap

--- funkolandia/settings.py
+++ funkolandia/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'funkolandia.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -117,4 +117,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+# STATIC_URL = '/static/'
+# STATIC_ROOT = BASE_DIR/ 'static'
+# STATICFILES_DIR = [
+#     'funkolandia/static',
+# ]
+
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+STATICFILES_DIRS = [
+    'funkolandia/static',
+]
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR,'static'),
+# )
